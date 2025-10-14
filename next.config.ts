94e91305463c9b119a05250b036b559bc07b8d74
Maycon henrique <mayconhs2007@gmail.com>
2025-10-14 09:25:15 -0300

    feito

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: "./journaline", // caminho relativo para a raiz real do projeto
+  },
 };
-
 export default nextConfig;
