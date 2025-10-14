9556cb8cd38d774c34debd150271aa6025186dbf
Maycon henrique <mayconhs2007@gmail.com>
2025-10-14 10:38:50 -0300

    salva

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@
 
 import Link from "next/link";
 import { motion } from "framer-motion";
-import Sidebar from "./components/Sidebar";
 import "./styles/globals.css";
-import styles from "./page.module.css"; // ✅ importar como módulo
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -15,9 +14,15 @@ export default function Home() {
           Um diário gamificado para estimular sua criatividade e progresso
           pessoal.
         </p>
-        <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-          <Link href="/login">
-            <button className="btn-primary">Ir para o login</button>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          <Link href="/login" passHref>
+            <button className="btn-primary">Começar a Jornada!</button>
           </Link>
         </motion.div>
       </main>
